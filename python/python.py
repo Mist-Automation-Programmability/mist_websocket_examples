b1bc0d763fd554b08515011d9758a119123d8d4d
Thomas Munzer <tmunzer@juniper.net>
2023-07-26 15:52:23 +0200

    update to load settings from env file

--- python/python.py
+++ python/python.py
@@ -1,17 +1,19 @@
-import base64
 import json
+import os
+from dotenv import load_dotenv
 
 import websocket  # websocket-client==0.44.0
 
-msg_received = 0
+MSG_RECEIVED = 0
 
-
-token = ''
-org_id = ''
-site_id = ''
+ENV_FILE = "~/.mist_env"
+MIST_HOST = "api.mist.com"
+MIST_APITOKEN = ''
+MIST_ORG_ID = ''
+MIST_SITE_ID = ''
 
 def on_message(ws, message):
-    global msg_received
+    global MSG_RECEIVED
     print('new message'.center(80, "-"))
     try:
         message_json = json.loads(message)
@@ -37,12 +39,38 @@ def on_close(wsapp, close_status_code, close_msg):
 
 def on_open(ws):
     print('onopen')
-    ws.send(json.dumps({'subscribe': f'/sites/{site_id}/stats/devices'}))
+    ws.send(json.dumps({'subscribe': f'/sites/{MIST_SITE_ID}/stats/devices'}))
+
+def _load_env(env_file:str, mist_host:str, mist_apitoken:str, mist_org_id:str, mist_site_id:str):
+    if env_file.startswith("~/"):
+        env_file = os.path.join(
+            os.path.expanduser("~"), env_file.replace("~/", "")
+        )
+    load_dotenv(dotenv_path=env_file, override=True)
+    if os.getenv("MIST_HOST"):
+        mist_host = os.getenv("MIST_HOST")
+    if os.getenv("MIST_APITOKEN"):
+        mist_apitoken = os.getenv("MIST_APITOKEN")
+    if os.getenv("MIST_ORG_ID"):
+        mist_org_id = os.getenv("MIST_ORG_ID")
+    if os.getenv("MIST_SITE_ID"):
+        mist_site_id = os.getenv("MIST_SITE_ID")
+    return mist_host, mist_apitoken, mist_org_id, mist_site_id
 
 
 if __name__ == "__main__":
-    ws = websocket.WebSocketApp("wss://api-ws.mist.com/api-ws/v1/stream",
-                                header={'Authorization': f'Token {token}'},
+    MIST_HOST, MIST_APITOKEN, MIST_ORG_ID, MIST_SITE_ID = _load_env(ENV_FILE, MIST_HOST, MIST_APITOKEN, MIST_ORG_ID, MIST_SITE_ID)
+    # host for websocket is api-ws.mist.com, so replacing "api." or "manage." with "api-ws." if not the right host
+    if MIST_HOST.startswith("api."):
+        MIST_HOST = MIST_HOST.replace("api.", "api-ws.")
+    elif MIST_HOST.startswith("manage."):
+        MIST_HOST = MIST_HOST.replace("manage.", "api-ws.")
+    print(f"MIST_HOST     : {MIST_HOST}")
+    print(f"MIST_APITOKEN : {MIST_APITOKEN[:6]}...{MIST_APITOKEN[-6:]}")
+    print(f"MIST_ORG_ID   : {MIST_ORG_ID}")
+    print(f"MIST_SITE_ID  : {MIST_SITE_ID}")
+    ws = websocket.WebSocketApp(f"wss://{MIST_HOST}/api-ws/v1/stream",
+                                header={'Authorization': f'Token {MIST_APITOKEN}'},
                                 on_message=on_message,
                                 on_error=on_error,
                                 on_close=on_close)
